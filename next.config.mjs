69a4ca57e1fd2d30036dff431e2a1c03fca48bfa
seokhun-eom <seokhun_eom@tmax.co.kr>
2024-08-14 19:47:45 +0900

    추가 라이브러리 설치

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,18 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
   compiler: {
     styledComponents: true,
   },
 };
+
+if (process.env.NODE_ENV === "production") {
+  nextConfig.compiler = {
+    styledComponents: true,
+    reactRemoveProperties: {
+      properties: [`^data-testid$`],
+    },
+  };
+}
 
 export default nextConfig;
